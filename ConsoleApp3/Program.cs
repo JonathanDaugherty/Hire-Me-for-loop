2a9d88b604568b8c715b98f8cd4cf6b0c8312d4f
jmdaugherty94@gmail.com <jmdaugherty94@gmail.com>
2021-07-20 11:56:44 -0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 1 == 0)
+                {
+                    Console.WriteLine("HIRE ME");
+                };
+            }
+        }
+    }
+}
